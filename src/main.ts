3600a898d8b29257840b9489687cf5d244759f95
MuradRashidov <muradrashidov995@gmail.com>
2026-07-01 23:49:17 +0400

    d4

--- src/main.ts
+++ src/main.ts
@@ -42,9 +42,13 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(3000, '0.0.0.0');
+ const port = process.env.PORT || 3000;
 
-  console.log('🚀 Server is running on http://localhost:3000');
+await app.listen(port);
+
+console.log(`🚀 Server is running on port ${port}`);
+
+  console.log(`🚀 Server is running on http://localhost:${port}`);
   process.on('unhandledRejection', (reason) => {
     console.error(reason);
   });
